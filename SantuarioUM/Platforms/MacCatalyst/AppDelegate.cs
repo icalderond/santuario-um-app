51a44b51cbf04fd103cbefaf642260a96ccf2678
Israel Calderon de la Cruz <icalderond@outlook.com>
2026-04-13 20:30:48 -0600

    First commit

---
+++ SantuarioUM/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SantuarioUM;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
