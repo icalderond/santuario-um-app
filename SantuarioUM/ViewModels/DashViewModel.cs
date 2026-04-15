ba034c2feb6abfc3b068675f5c6ad1d116ef7dc3
Israel Calderon de la Cruz <icalderond@outlook.com>
2026-04-15 08:14:16 -0600

    + navigation to parent article

--- SantuarioUM/ViewModels/DashViewModel.cs
+++ SantuarioUM/ViewModels/DashViewModel.cs
@@ -1,10 +1,38 @@
+using CommunityToolkit.Mvvm.Input;
 using SantuarioUM.Utilities.Interfaces;
 
 namespace SantuarioUM.ViewModels;
 
-public class DashViewModel : IDashViewModel
+public partial class DashViewModel : IDashViewModel
 {
-    public DashViewModel()
+    #region Private Properties
+
+    private readonly INavigationService _navigationService;
+    #endregion
+    
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="navigationService"></param>
+    public DashViewModel(INavigationService navigationService)
     {
+        _navigationService = navigationService;
     }
+
+    #region Methods
+
+    [RelayCommand]
+    private async void GoToAtrioAsync()=>   GoToNavigation(0);
+    [RelayCommand]
+    private async void GoToSantoAsync()=>   GoToNavigation(1);
+    [RelayCommand]
+    private async void GoToSantisimoAsync()=>   GoToNavigation(2);
+    
+
+    private async Task GoToNavigation(int i)
+    {
+        await _navigationService.ShellGoToAsync($"{nameof(ParentArticleViewModel)}?parentArticleId={i}");
+    }
+
+    #endregion
 }
