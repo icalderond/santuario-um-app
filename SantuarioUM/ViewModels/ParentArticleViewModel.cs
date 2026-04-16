0753eda594b95555285a399543f8e648668b79a8
Israel Calderon de la Cruz <icalderond@outlook.com>
2026-04-15 23:51:07 -0600

    + UI for ParentArticlePage.xaml

--- SantuarioUM/ViewModels/ParentArticleViewModel.cs
+++ SantuarioUM/ViewModels/ParentArticleViewModel.cs
@@ -1,8 +1,35 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using SantuarioUM.Models;
 using SantuarioUM.Utilities.Interfaces;
 
 namespace SantuarioUM.ViewModels;
 
-public class ParentArticleViewModel : IParentArticleViewModel
+public partial class ParentArticleViewModel : BaseViewModel, IParentArticleViewModel, IQueryAttributable
 {
-    
+    #region Private Properties
+
+    private readonly ISantuarioService _santuarioService;
+    [ObservableProperty] private Area? _currentArea;
+
+    #endregion
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="santuarioService"></param>
+    public ParentArticleViewModel(ISantuarioService santuarioService)
+    {
+        _santuarioService = santuarioService;
+    }
+
+    public async void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("area"))
+        {
+            var area = query["area"].ToString();
+
+            var santuarioData = await _santuarioService.GetSantuarioData();
+            CurrentArea = santuarioData.Areas.FirstOrDefault(a => a.Id == area);
+        }
+    }
 }
