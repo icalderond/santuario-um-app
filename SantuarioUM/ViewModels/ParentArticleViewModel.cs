23552842059fdd8c26310114cd888fd68185edfb
Israel Calderon de la Cruz <icalderond@outlook.com>
2026-04-16 08:18:10 -0600

    + UI for ParentArticlePage.xaml

--- SantuarioUM/ViewModels/ParentArticleViewModel.cs
+++ SantuarioUM/ViewModels/ParentArticleViewModel.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using SantuarioUM.Models;
 using SantuarioUM.Utilities.Interfaces;
 
@@ -8,7 +9,9 @@ public partial class ParentArticleViewModel : BaseViewModel, IParentArticleViewM
 {
     #region Private Properties
 
+    string _areaId =  string.Empty;
     private readonly ISantuarioService _santuarioService;
+    private readonly INavigationService _navigationService;
     [ObservableProperty] private Area? _currentArea;
 
     #endregion
@@ -17,19 +20,34 @@ public partial class ParentArticleViewModel : BaseViewModel, IParentArticleViewM
     /// Constructor
     /// </summary>
     /// <param name="santuarioService"></param>
-    public ParentArticleViewModel(ISantuarioService santuarioService)
+    /// <param name="navigationService"></param>
+    public ParentArticleViewModel(
+        ISantuarioService santuarioService, 
+        INavigationService navigationService)
     {
         _santuarioService = santuarioService;
+        _navigationService = navigationService;
     }
 
+    /// <summary>
+    /// Apply query attributes
+    /// </summary>
+    /// <param name="query"></param>
     public async void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         if (query.ContainsKey("area"))
         {
             var area = query["area"].ToString();
-
+            _areaId = area ?? string.Empty;
+            
             var santuarioData = await _santuarioService.GetSantuarioData();
             CurrentArea = santuarioData.Areas.FirstOrDefault(a => a.Id == area);
         }
+    }
+
+    [RelayCommand]
+    private async Task NavigateToAsync(string muebleId)
+    {
+        await _navigationService.ShellGoToAsync($"{nameof(ArticleViewModel)}?area={_areaId}&mueble={muebleId}");
     }
 }
