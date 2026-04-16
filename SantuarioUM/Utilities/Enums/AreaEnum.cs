766fb45ae8dce4b41cdaa777a6a8f6404928ff6e
Israel Calderon de la Cruz <icalderond@outlook.com>
2026-04-15 23:49:08 -0600

    + AreaEnum.cs and MuebleEnum.cs and EnumExtensions.cs

---
+++ SantuarioUM/Utilities/Enums/AreaEnum.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel;
+
+namespace SantuarioUM.Utilities.Enums;
+
+public enum AreaEnum
+{
+    [Description("atrio")] Atrio = 1,
+
+    [Description("lugar_santo")] LugarSanto = 2,
+
+    [Description("lugar_santisimo")] LugarSantisimo = 3
+}
---
+++ SantuarioUM/Utilities/Enums/MuebleEnum.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel;
+
+namespace SantuarioUM.Utilities.Enums;
+
+public enum MuebleEnum
+{
+    [Description("altar_del_sacrificio")] AltarSacrificio = 1,
+
+    [Description("lavacro")] Lavacro = 2,
+
+    [Description("mesa_panes")] MesaPanes = 2,
+
+    [Description("candelabro")] Candelabro = 2,
+
+    [Description("altar_incienso")] AltarIncienso = 2,
+
+    [Description("arca_pacto")] ArcaPacto = 2,
+}
