9c633bb6c49132f65f73c140c9ae60e058848e92
Israel Calderon de la Cruz <icalderond@outlook.com>
2026-04-17 23:45:31 -0500

    + image for Area and mueble

--- SantuarioUM/Models/SantuarioData.cs
+++ SantuarioUM/Models/SantuarioData.cs
@@ -14,6 +14,7 @@ public class Area
 {
     public string Id { get; set; }
     public string Titulo { get; set; }
+    public string Imagen { get; set; }
     public string Descripcion { get; set; }
     public List<Mueble> Muebles { get; set; }
 }
@@ -22,6 +23,7 @@ public class Mueble
 {
     public string Id { get; set; }
     public string Titulo { get; set; }
+    public string Imagen { get; set; }
     public Dimensiones Dimensiones { get; set; }
 }
 
